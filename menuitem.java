9ef0f6231534d6da818add5a2f0b9e84856d1c1b
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-03-09 00:49:19 +0530

    menuitem.java

---
+++ menuitem.java
@@ -0,0 +1,34 @@
+class MenuItem {
+    private String name;
+    private double price;
+
+    public MenuItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0) {
+            this.price = price;
+        } else {
+            System.out.println("Price must be positive.");
+        }
+    }
+
+    public String toString() {
+        return name + " ($" + price + ")";
+    }
+}
+
+
